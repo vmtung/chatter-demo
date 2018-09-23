7dbca83f5d38e7489782069edb17cfe843a08932
Tung Vu <vmtung93@gmail.com>
2018-09-24 01:22:42 +0700

    adapt express setup to new configs, prepare for prod, single page app

--- server/config/express.js
+++ server/config/express.js
@@ -1,26 +1,41 @@
-const express = require('express');
-const consign = require('consign');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path');
+const express = require('express')
+const consign = require('consign')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const path = require('path')
+const compression = require('compression')
 
 module.exports = () => {
-  const app = express();
+  const app = express()
 
-  app.set('port', (process.env.PORT || 3001));
-  app.set('client', path.resolve(__dirname, '../../client/dist'));
-  
-  app.use(cors());
-  app.use(express.static(app.get('client')));
-  app.use(bodyParser.urlencoded({extended: true}));
-  app.use(bodyParser.json());
-  
-  consign({cwd: 'server/app'})
+  app.use(compression())
+  app.set('port', process.env.PORT || 3001)
+  if (process.env.NODE_ENV !== 'production') {
+    app.set('client-path', '/')
+    app.set('client', path.resolve(__dirname, '../../client/dist'))
+    app.set('index', path.resolve(__dirname, '../../client/dist/index.html'))
+  } else {
+    // app.set('client', path.resolve(__dirname, '../../'))
+    app.set('client-path', '/client')
+    app.set('client', path.resolve(__dirname, '../../client/build'))
+    app.set('index', path.resolve(__dirname, '../../client/build/index.html'))
+  }
+
+  app.use(cors())
+  app.use(app.get('client-path'), express.static(app.get('client')))
+  app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(bodyParser.json())
+
+  consign({ cwd: 'server/app' })
     .include('models')
     .include('controllers')
+    .include('middlewares')
     .then('routes')
 
-    .into(app);
+    .into(app)
+  app.get('*', function response(req, res) {
+    res.sendFile(app.get('index'))
+  })
 
-  return app;
+  return app
 }
