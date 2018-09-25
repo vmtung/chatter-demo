f9e391d70b82660d03776e7896a7195a77847753
Tung Vu <vmtung93@gmail.com>
2018-09-25 14:42:02 +0700

    fix static client assets path for single page app

--- server/config/express.js
+++ server/config/express.js
@@ -10,19 +10,17 @@ module.exports = () => {
 
   app.use(compression())
   app.set('port', process.env.PORT || 3001)
+
   if (process.env.NODE_ENV !== 'production') {
-    app.set('client-path', '/')
     app.set('client', path.resolve(__dirname, '../../client/dist'))
     app.set('index', path.resolve(__dirname, '../../client/dist/index.html'))
   } else {
-    // app.set('client', path.resolve(__dirname, '../../'))
-    app.set('client-path', '/client')
     app.set('client', path.resolve(__dirname, '../../client/build'))
     app.set('index', path.resolve(__dirname, '../../client/build/index.html'))
   }
 
   app.use(cors())
-  app.use(app.get('client-path'), express.static(app.get('client')))
+  app.use('/client', express.static(app.get('client')))
   app.use(bodyParser.urlencoded({ extended: true }))
   app.use(bodyParser.json())
 
