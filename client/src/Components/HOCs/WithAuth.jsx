497985217c9a04d0c2acf34837dc637b88cc2834
Tung <vmtung93@gmail.com>
2019-02-21 15:04:05 +0700

    remove todo

--- client/src/Components/HOCs/WithAuth.jsx
+++ client/src/Components/HOCs/WithAuth.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { onAuthChanged } from '../../helpers/Auth'
-// TODO: check local first, then remote, if mismatch then update like remote
+
 export default function withAuth(Comp) {
   return class authHOC extends Component {
     state = {
